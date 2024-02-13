036e87d4b2479024b5ae9e6d57793a86f5c77146
Alexey Ses <alexey.ses@eijsink.com>
2024-02-13 15:14:49 +0100

    use "lit" instead of "lit-html" package, add header component

--- src/index.js
+++ src/index.js
@@ -1,23 +1,12 @@
-/**
- * @license
- * Copyright (c) 2020 The Polymer Project Authors. All rights reserved.
- * This code may only be used under the BSD style license found at
- * http://polymer.github.io/LICENSE.txt
- * The complete set of authors may be found at
- * http://polymer.github.io/AUTHORS.txt
- * The complete set of contributors may be found at
- * http://polymer.github.io/CONTRIBUTORS.txt
- * Code distributed by Google as part of the polymer project is also
- * subject to an additional IP rights grant found at
- * http://polymer.github.io/PATENTS.txt
- */
-import { html, render } from 'lit-html';
+import { html, render } from 'lit';
+import './some-header.js';
 
 const container = document.querySelector('#main');
 
 const todos = ['Buy hand sanitizer', 'Shred cabbage', 'Learn TypeScript'];
 
 const template = () => html`
+    <some-header></some-header>
     <h2>To Do</h2>
     <ul>
         ${todos.map((todo) => html`
---
+++ src/some-header.js
@@ -0,0 +1,10 @@
+import { LitElement, html } from 'lit';
+
+class SomeHeader extends LitElement {
+  render() {
+    return html`
+        <header>lit-and-rollup-test app</header>
+    `;
+  }
+}
+customElements.define('some-header', SomeHeader);
